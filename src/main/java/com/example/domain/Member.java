d27664fadfba13450d4fe8b78ff659b3c6f9ccef
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-06-16 15:34:27 +0900

    domain更新

--- src/main/java/com/example/domain/Member.java
+++ src/main/java/com/example/domain/Member.java
@@ -3,26 +3,36 @@ package com.example.domain;
 public class Member {
 
 	private String name;
-
+	private Integer age;
+	private Integer depId;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + ", depId=" + depId + "]";
+	}
+	
 	public String getName() {
 		return name;
 	}
-	
-	
-	
-
-	@Override
-	public String toString() {
-		return "Member [name=" + name + "]";
-	}
-
-
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Integer getDepId() {
+		return depId;
+	}
+	public void setDepId(Integer depId) {
+		this.depId = depId;
+	}
 	
 	
+
 	
 }
