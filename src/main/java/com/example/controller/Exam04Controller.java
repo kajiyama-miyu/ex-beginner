3941bb6d54ecd4086b412f44b0e7c23b0055a138
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-06-16 13:47:37 +0900

    controller追加

---
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -0,0 +1,38 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.User;
+import com.example.form.UserForm;
+
+@Controller
+@RequestMapping("exam04")
+public class Exam04Controller {
+
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
+	
+	@RequestMapping("")
+	public String index() {
+		return "exam04";
+	}
+	
+	@RequestMapping("/input-user")
+		public String inputUser(UserForm userForm, Model model) {
+			User user = new User();
+			user.setName(userForm.getName());
+			user.setAge(userForm.getAge());
+			user.setComment(userForm.getComment());
+			
+			model.addAttribute("user", user);
+			
+			return "exam04-result";
+		}
+	}
+
+
