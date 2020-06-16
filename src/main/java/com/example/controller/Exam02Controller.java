657dbd126bc4f5881ee0209001c95e545ce46e65
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-06-16 11:13:49 +0900

    Controller変更

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,17 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/exam02")
 public class Exam02Controller {
 
+	@Autowired
+	public HttpSession session;
+	
 	@RequestMapping("")
 	public String index() {
 		return "exam02";
 	}
 	
 	@RequestMapping("/calc")
-	public String calc(Integer num1, Integer num2, Model model) {
-		model.addAttribute("num1", num1);
-		model.addAttribute("num2", num2);
+	public String calc(Integer num1, Integer num2) {
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
 		int answer = num1+ num2;
-		model.addAttribute("answer", answer);
+		session.setAttribute("answer", answer);
 		
 		return "exam02-result";
 	}
